634f11bec9962ad8dccf8a1ee543723b4ba51b30
Xandra Campo <xkmpera@gmail.com>
2024-09-23 15:33:28 +0200

    Re-define views

--- uspekpy-web/__init__.py
+++ uspekpy-web/__init__.py
@@ -8,11 +8,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/uspekpy')
-def uspekpy():
-    return render_template('uspekpy.html')
+@app.route('/single-case')
+def single():
+    return render_template('single.html')
 
 
-@app.route('/user-guide')
-def user_guide():
-    return render_template('user_guide.html')
+@app.route('/batch-cases')
+def batch():
+    return render_template('batch.html')
